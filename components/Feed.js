3926102fd42921b3152b4ea31be022ea999ba0e6
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 18:34:23 +0900

    input box

--- components/Feed.js
+++ components/Feed.js
@@ -1,3 +1,4 @@
+import InputBox from "./InputBox"
 import Stories from "./Stories"
 
 function Feed() {
@@ -5,6 +6,7 @@ function Feed() {
     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories/>
+        <InputBox/>
       </div>
     </div>
   )
