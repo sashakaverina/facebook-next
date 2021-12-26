cef094840eb9ad164fd1b7aa82f31d41bc78fcfe
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 21:08:06 +0900

    footer for the post

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import { ChatAltIcon, ChatIcon, ShareIcon, ThumbUpIcon } from "@heroicons/react/solid"
 
 function Post( { name, message, email, timestamp, postImage, image}) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col rounded-2xl">
       <div className="p-5 bg-white mt-5 rounded-2xl shadow-sm">
         <div className="flex items-center space-x-2">
           <img src={image} width={40} height={40} className="rounded-full"/>
@@ -22,6 +23,23 @@ function Post( { name, message, email, timestamp, postImage, image}) {
           layout="fill"/>
         </div>
       )}
+
+      <div className="flex justify-between items-center rounded-b-2x
+      shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ThumbUpIcon className="h-4"/>
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none">
+          <ChatIcon className="h-4"/>
+          <p className="text-xs sm:text-base">Comment</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ShareIcon className="h-4"/>
+          <p className="text-xs sm:text-base">Share</p>
+        </div>
+
+      </div>
     </div>
   )
 }
