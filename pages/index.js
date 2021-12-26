d9ad0309ccd36502bd5b0bef9bb3564f0850dc68
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 16:14:23 +0900

    next-auth done

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import { getSession } from "next-auth/react"
+import Login from '../components/Login'
 
 
-export default function Home() {
+
+export default function Home( {session }) {
+  if (!session) return <Login/>
   return (
     <div>
       <Head>
@@ -12,4 +16,13 @@ export default function Home() {
       <Header/>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  }
 }
