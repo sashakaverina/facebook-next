ab673a377bb939ed224efb1d2ff54fc26977775b
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 14:56:04 +0900

    right header/profile

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ function Header() {
           <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
             <SearchIcon className="h-6 "/>
             <input
-             className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+             className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
              type="text"
              placeholder="Search Facebook"
             />
@@ -38,13 +38,20 @@ function Header() {
       </div>
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon active Icon={HomeIcon} />
           <HeaderIcon Icon={FlagIcon} />
           <HeaderIcon Icon={PlayIcon} />
           <HeaderIcon Icon={ShoppingCartIcon} />
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
-
+      </div>
+      <div className="flex items-center sm:space-x-2 justify-end">
+        
+        <p className="whitespace-nowrap font-semibold pr-3">Sasha K</p>
+        <ViewGridIcon className="icon"/>
+        <ChatIcon className="icon"/>
+        <BellIcon className="icon"/>
+        <ChevronDownIcon className="icon"/>
       </div>
 
     </div>
