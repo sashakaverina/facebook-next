e8a628101e16883ff28d821bdf59ddebafd6387a
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 13:20:15 +0900

    upgraded nodes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, 
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [], 
+};
