38f9095baf3df3e340ac519d7fd03985aa500f18
Sasha Kaverina <sasha.kaverina@lewagon.org>
2021-12-26 17:00:24 +0900

    session pic and name

--- components/Header.js
+++ components/Header.js
@@ -14,9 +14,13 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import HeaderIcon from "./HeaderIcon";
+import { useSession, signOut } from "next-auth/react";
+
 
 
 function Header() {
+  const { data: session, status } = useSession();
+
   return (
     <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       <div className="flex items-center">
@@ -45,9 +49,19 @@ function Header() {
           <HeaderIcon Icon={UserGroupIcon} />
         </div>
       </div>
+
       <div className="flex items-center sm:space-x-2 justify-end">
+        <Image
+        onClick={signOut}
+        className="rounded-full cursor-pointer"
+        alt="sign out"
+        src={session.user.image}
+        width={40}
+        height={40}
+        layout="fixed"
+         />
         
-        <p className="whitespace-nowrap font-semibold pr-3">Sasha K</p>
+        <p className="whitespace-nowrap font-semibold pr-3">{session.user.name}</p>
         <ViewGridIcon className="icon"/>
         <ChatIcon className="icon"/>
         <BellIcon className="icon"/>
